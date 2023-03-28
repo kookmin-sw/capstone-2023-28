922f0ffe2ab23f43c4e9453b72d4d1b66bf358b5
minjun7410 <minjun7410@gmail.com>
2023-03-28 22:57:03 +0900

    [django-server] Follow 필드 변경 (user_follow_id의 속성을 CharField -> ForeignKey)

--- back/python/django_server/database/authentication/models.py
+++ back/python/django_server/database/authentication/models.py
@@ -48,30 +48,10 @@ class User(AbstractBaseUser):
         super().save(*args, **kwargs)
 
 
-
-from django.db import models
-
-
-
 # follow unfollow
-
-class User(models.Model):
-    user_id = models.IntegerField()
-    user_nickname = models.CharField(max_length=45)
-    user_definition = models.CharField(max_length=45, blank=True, null=True)
-    user_profile_image = models.CharField(max_length=45)
-    user_point_number = models.CharField(max_length=45)
-    is_seller = models.BigIntegerField()
-
-    class Meta:
-        db_table = "user"
-    
-    def __str__(self):
-        return self.user_nickname
-    
 class Follow(models.Model):
-    following_user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    follow_user_id = models.CharField(max_length=45)
+    following_user_id = models.ForeignKey('User', on_delete=models.CASCADE, related_name="user_following_set")
+    follow_user_id = models.ForeignKey('User', on_delete=models.CASCADE, related_name="user_follow_set")
 
     class Meta:
         db_table = "follow"
