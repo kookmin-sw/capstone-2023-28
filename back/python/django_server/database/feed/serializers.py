f35bc9f91a2d2598a021917bfe6e47d5ee95c4ff
minjun7410 <minjun7410@gmail.com>
2023-04-25 20:14:51 +0900

    [django-server] 피드 가져올 때 이미지를 URL에서 바이너리 코드로 줌.

--- back/python/django_server/database/feed/serializers.py
+++ back/python/django_server/database/feed/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import *
 from authentication.models import User
+from authentication.serializers import UserSerializer
 from API.s3 import S3ImageUploader
 from database import settings
 import boto3
+import base64
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
@@ -24,23 +26,19 @@ class FeedImageSerializer(serializers.ModelSerializer):
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             region_name=settings.AWS_S3_REGION_NAME
         )
-        url = s3_client.generate_presigned_url(
-            'get_object',
-            Params={
-                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
-                'Key': ret['image'],
-            }
-        )
-        ret['image'] = url
+        body = s3_client.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME ,Key=ret['image'])["Body"]
+        raw_data = body.read()
+        ret['image'] = base64.b64encode(raw_data).decode('utf-8')
         del ret['feed_id']
         return ret
 
 class FeedSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     images = FeedImageSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Feed
-        fields = ("feed_id", "user_id", "content", "created_at", "updated_at", "comments", "images")
+        fields = ("feed_id", "user_id", "content", "created_at", "updated_at", "comments", "images", "user")
         extra_kwargs = {"user_id": {"required": False}}
     def create(self, validated_data):
         user_id = self.context.get("user_id")
