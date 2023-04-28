993b566389a29c6a0e0dfb71636a6e5798540ea6
20181625 <20181625@kookmin.ac.kr>
2023-04-28 17:06:01 +0900

    Updated news_alert.py.

--- back/python/aws_lambda/news/news_alert.py
+++ back/python/aws_lambda/news/news_alert.py
@@ -1,7 +1,9 @@
 import requests
 import time
 from typing import List
-from utils.key import CLIENT_ID, CLIENT_SECRET
+from utils.key import CLIENT_SECRET, CLIENT_ID
+import threading
+
 class Observer:
     def update(self, *args, **kwargs):
         pass
@@ -12,8 +14,26 @@ class NaverNewsObserver(Observer):
     
     def update(self, news):
         if news != self.last_news:
-            print(f"New article found: {news['title']} - {news['originallink']}")
+            print(f"New news article found: {news['title']} - {news['originallink']}")
             self.last_news = news
+
+class NaverBlogObserver(Observer):
+    def __init__(self):
+        self.last_blog = None
+    
+    def update(self, blog):
+        if blog != self.last_blog:
+            print(f"New blog article found: {blog['title']} - {blog['link']}")
+            self.last_blog = blog
+
+class NaverCafeObserver(Observer):
+    def __init__(self):
+        self.last_cafe = None
+    
+    def update(self, cafe):
+        if cafe != self.last_cafe:
+            print(f"New cafe article found: {cafe['title']} - {cafe['link']}")
+            self.last_cafe = cafe
 
 class NaverSearchAPI:
     def __init__(self, keyword: str):
@@ -26,9 +46,14 @@ class NaverSearchAPI:
     def detach(self, observer: Observer):
         self.observers.remove(observer)
     
-    def notify_observers(self, news):
+    def notify_observers(self, article, article_type):
         for observer in self.observers:
-            observer.update(news)
+            if article_type == "news":
+                observer.update(article)
+            elif article_type == "blog":
+                observer.update(article)
+            elif article_type == "cafe":
+                observer.update(article)
     
     def search_news(self):
         while True:
@@ -40,13 +65,52 @@ class NaverSearchAPI:
             params = {"query": self.keyword}
             response = requests.get(url, headers=headers, params=params)
             if response.status_code == 200:
-                for news in response.json()["items"]:
-                    if "호선" in news["title"]:
-                        self.notify_observers(news)
+                for article in response.json()["items"]:
+                    if "호선" in article["title"]:
+                        self.notify_observers(article, "news")
+            time.sleep(10)
+    
+    def search_blog(self):
+        while True:
+            url = "https://openapi.naver.com/v1/search/blog.json"
+            headers = {
+                "X-Naver-Client-Id": CLIENT_ID,
+                "X-Naver-Client-Secret": CLIENT_SECRET
+            }
+            params = {"query": self.keyword}
+            response = requests.get(url, headers=headers, params=params)
+            if response.status_code == 200:
+                for article in response.json()["items"]:
+                    if "호선" in article["title"]:
+                        self.notify_observers(article, "blog")
+            time.sleep(10)
+    
+    def search_cafe(self):
+        while True:
+            url = "https://openapi.naver.com/v1/search/cafearticle.json"
+            headers = {
+                "X-Naver-Client-Id": CLIENT_ID,
+                "X-Naver-Client-Secret": CLIENT_SECRET
+            }
+            params = {"query": self.keyword}
+            response = requests.get(url, headers=headers, params=params)
+            if response.status_code == 200:
+                for article in response.json()["items"]:
+                    if "호선" in article["title"]:
+                        self.notify_observers(article, "cafe")
             time.sleep(10)
 
 if __name__ == "__main__":
+    observer1 = NaverNewsObserver()
+    observer2 = NaverBlogObserver()
+    observer3 = NaverCafeObserver()
     naver_api = NaverSearchAPI("호선")
-    observer = NaverNewsObserver()
-    naver_api.attach(observer)
-    naver_api.search_news()
+    naver_api.attach(observer1)
+    naver_api.attach(observer2)
+    naver_api.attach(observer3)
+    thread_news = threading.Thread(target=naver_api.search_news)
+    thread_blog = threading.Thread(target=naver_api.search_blog)
+    thread_cafe = threading.Thread(target=naver_api.search_cafe)
+    thread_news.start()
+    thread_blog.start()
+    thread_cafe.start()
