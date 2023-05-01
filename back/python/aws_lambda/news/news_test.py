145033ab37097e3dd01020b81a0e85b806b737ef
20181625 <20181625@kookmin.ac.kr>
2023-05-02 04:38:52 +0900

    Edited test code.

--- back/python/aws_lambda/news/news_test.py
+++ back/python/aws_lambda/news/news_test.py
@@ -1,21 +1,9 @@
 import requests
 import json
 import time
-from datetime import datetime, timedelta
 from typing import List
+from datetime import datetime, timedelta
 from utils.key import CLIENT_ID, CLIENT_SECRET
-
-class Singleton(type):
-    """
-    This is a Singleton metaclass that ensures only one instance of a class is created.
-    """
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
-        return cls._instances[cls]
-
 
 class News:
     def __init__(self):
@@ -37,13 +25,13 @@ class Subscriber:
         pass
 
 
-class NaverNewsAPI(metaclass=Singleton):
+class NaverNewsAPI:
     def __init__(self):
         self.url = "https://openapi.naver.com/v1/search/news.json"
         self.headers = {"X-Naver-Client-Id": CLIENT_ID, "X-Naver-Client-Secret": CLIENT_SECRET}
 
-    def search_news(self, query: str) -> List[dict]:
-        params = {"query": query, "display": 100}
+    def search_news(self, query: str, start_date: str, end_date: str) -> List[dict]:
+        params = {"query": query, "display": 100, "sort": "date", "start": 1, "start_date": start_date, "end_date": end_date}
         res = requests.get(self.url, headers=self.headers, params=params)
         if res.status_code == 200:
             data = json.loads(res.text)
@@ -51,30 +39,11 @@ class NaverNewsAPI(metaclass=Singleton):
                 return data["items"]
         return []
 
-    def get_recent_news(self, query: str, hours: int = 24, count: int = 3) -> List[dict]:
-        now = datetime.now()
-        target_time = now - timedelta(hours=hours)
-        items = []
-        page = 1
-        while len(items) < count:
-            params = {"query": query, "display": 100, "sort": "date", "start": (page - 1) * 100 + 1}
-            res = requests.get(self.url, headers=self.headers, params=params)
-            if res.status_code == 200:
-                data = json.loads(res.text)
-                if "items" in data:
-                    for item in data["items"]:
-                        pub_date = datetime.strptime(item["pubDate"], "%a, %d %b %Y %H:%M:%S +0900")
-                        if pub_date < target_time:
-                            # articles are sorted by date, so we can break once we reach an old article
-                            break
-                        items.append(item)
-                        if len(items) == count:
-                            break
-            page += 1
-        return items
-
 
 class NewsWatcher(Subscriber):
+    def __init__(self, keyword):
+        self.keyword = keyword
+
     def update(self, message):
         for item in message:
             print(f"New News Found: {item['title']} ({item['link']})")
@@ -83,13 +52,19 @@ class NewsWatcher(Subscriber):
 def main():
     news = News()
     api = NaverNewsAPI()
-    watcher = NewsWatcher()
-    news.subscribe(watcher)
+    watchers = [NewsWatcher("호선"), NewsWatcher("지하철")]
+    for watcher in watchers:
+        news.subscribe(watcher)
 
     while True:
-        items = api.get_recent_news("", hours=24, count=3)
-        news.notify(items)
+        end_date = datetime.now().strftime('%Y-%m-%d')
+        start_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        for watcher in watchers:
+            items = api.search_news(watcher.keyword, start_date, end_date)[:3] # get only 3 recent news
+            news.notify(items)
+        print("Waiting 60 seconds...")
         time.sleep(60)
+        
 
 
 if __name__ == '__main__':
