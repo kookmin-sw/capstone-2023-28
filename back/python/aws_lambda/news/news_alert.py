24c7d480a94a96ae663a709fe56e0b48efd7dd81
20181625 <20181625@kookmin.ac.kr>
2023-05-02 11:03:34 +0900

    Need changes.

--- back/python/aws_lambda/news/news_alert.py
+++ back/python/aws_lambda/news/news_alert.py
@@ -1,9 +1,9 @@
 import requests
 import json
 import time
+from datetime import datetime, timedelta
 from typing import List
 from utils.key import CLIENT_ID, CLIENT_SECRET
-from datetime import datetime, timedelta
 
 class News:
     def __init__(self):
@@ -31,10 +31,7 @@ class NaverNewsAPI:
         self.headers = {"X-Naver-Client-Id": CLIENT_ID, "X-Naver-Client-Secret": CLIENT_SECRET}
 
     def search_news(self, query: str) -> List[dict]:
-        end_date = datetime.now().strftime('%Y-%m-%d')
-        start_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-        params = {"query": query, "display": 3, "sort": "date", "start_date": start_date, "end_date": end_date}
-        res = requests.get(self.url, headers=self.headers, params=params)
+        res = requests.get(self.url, headers=self.headers, params=query)
         if res.status_code == 200:
             data = json.loads(res.text)
             if "items" in data:
@@ -43,6 +40,9 @@ class NaverNewsAPI:
 
 
 class NewsWatcher(Subscriber):
+    def __init__(self, keyword):
+        self.keyword = keyword
+
     def update(self, message):
         for item in message:
             print(f"New News Found: {item['title']} ({item['link']})")
@@ -51,31 +51,28 @@ class NewsWatcher(Subscriber):
 def main():
     news = News()
     api = NaverNewsAPI()
-    watcher = NewsWatcher()
-    news.subscribe(watcher)
 
-    while True:
-        items = api.search_news("")
-        news.notify(items)
-        time.sleep(60)
+    # Get latest 3 articles from last 24 hours
+    now = datetime.now()
+    query = {"query": "", "display": 100, "sort": "date"}
+    query["query"] = f"datetime:{(now - timedelta(days=1)).strftime('%Y-%m-%d')}:{now.strftime('%Y-%m-%d')}"
+    items = api.search_news(query)[:3]
+    news.notify(items)
 
-
-
-
-def main2():
-    news = News()
-    api = NaverNewsAPI()
+    # Watch for new articles
     watchers = [NewsWatcher("호선"), NewsWatcher("지하철")]
     for watcher in watchers:
         news.subscribe(watcher)
 
     while True:
         for watcher in watchers:
-            items = api.search_news(watcher.keyword)
+            query = {"query": watcher.keyword, "display": 1, "sort": "date"}
+            items = api.search_news(query)
             news.notify(items)
 
+        print("Wainting for 60 seconds... \n")
         time.sleep(60)
-
+        
 
 if __name__ == '__main__':
     main()
