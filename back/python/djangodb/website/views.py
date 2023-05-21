a8f3f74c931ab7a8a1f00a294c3ae9a6b6bf4c3f
송민준 <minjun7410@kookmin.ac.kr>
2023-05-21 11:34:40 +0900

    Delete back/python/djangodb directory

--- back/python/djangodb/website/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import User, Follow
-
-def home(request):
-    all_user = User.objects.all
-    all_follow = Follow.objects.all
-    return render(request, 'home.html', {'all_user':all_user, 'all_follow':all_follow})
